1c716f755205b39a3a2d4c421d34c0be21ac2173
Nathan Guerroudj <nathan.g2710@gmail.com>
2023-03-30 09:20:36 +0200

    connexion

--- cgu.php
+++ cgu.php
@@ -1,5 +1,17 @@
 <?php
 
+// Démarrage de la session
+session_start();
+
+// Inclusion de la config
+require_once 'config.php';
+
+// Inclusion des dépendances
+require 'fonction.php';
+
+// Connexion à la base de données
+$pdo = getPdoConnection();
+
 // Affichage : inclusion du template
 $template = 'cgu';
 include 'templates/base.phtml';
--- mention.php
+++ mention.php
@@ -1,5 +1,17 @@
 <?php
 
+// Démarrage de la session
+session_start();
+
+// Inclusion de la config
+require_once 'config.php';
+
+// Inclusion des dépendances
+require 'fonction.php';
+
+// Connexion à la base de données
+$pdo = getPdoConnection();
+
 // Affichage : inclusion du template
 $template = 'mention';
 include 'templates/base.phtml';
